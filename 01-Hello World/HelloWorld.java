9eea6e55e7705247120a3ee5c2dd9456474eb431
sathishinti <sathishsarma.inti@gmail.com>
2025-08-21 13:51:19 +0800

    Update HelloWorld.java

--- 01-Hello World/HelloWorld.java
+++ 01-Hello World/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
     // Prints "Hello, World" to the terminal window.
     public static void main(String[] args)
     {
-        System.out.println("Hello, World");
+        System.out.println("Content is changed");
+        
     }
 }
